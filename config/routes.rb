0c42315898f1a384716a89ca6bd36945ccdb3eef
gbagaev <gbagaev@gmail.com>
2018-06-23 13:40:05 -0700

    clean ClientController and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  resources :clients
+  resources :clients, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root "clients#new"
 end
